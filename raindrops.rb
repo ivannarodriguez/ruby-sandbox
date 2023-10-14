5664cba8ed427c8c53b474ec387d1a36fbe8c793
Ivanna Rodríguez Lobo <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-14 20:01:20 +0000

    raindrops

---
+++ raindrops.rb
@@ -0,0 +1,14 @@
+integers = [1, 21, 35, 105]
+integer = integers.sample
+
+if integer % 3 == 0 && integer % 5 == 0 && integer % 7 == 0
+  pp "PlingPlangPlong"
+elsif integer % 3 == 0
+  pp "Pling"
+elsif integer % 5 == 0
+  pp "Plang"
+elsif integer % 7 == 0
+  pp "Plong"
+else
+  pp integer
+end
